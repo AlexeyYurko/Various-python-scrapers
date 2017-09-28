a22c455b0f789fb3aeef1e9b018d751d9aeb1524
Alexey Yurko <a.yurko@gmail.com>
2017-09-28 18:37:32 +0300

    Initial commit

---
+++ filfre/README.md
@@ -0,0 +1,11 @@
+# Get articles from filfre.net
+
+####filfre.net is very well written digital almanac of gaming history by Jimmy Maher (http://www.filfre.net/about-me/).
+####I want to load all articles, images (see python files) and translate it to russian language in matter of some facts:
+###1. I really like it.
+###2. I want keep my english on certain level.
+
+MongoDB just for trying NoSQL, nothing serious.
+
+You need the img folder in place where you start the python file.
+After thousand and some hundreds images the way of linking images was changed - i'm not proud of scraping realisation - so, to be short, just run 'correct_images.py'
---
+++ filfre/filfre_articles_m.py
@@ -0,0 +1,62 @@
+"""
+grab list of articles, articles itself and images from filfre.net
+using MongoDB
+"""
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import requests
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client['filfre']
+collection = db['articles']
+
+image_count = 0
+
+url = ("http://www.filfre.net/sitemap/")
+page = urlopen(url)
+articles = BeautifulSoup(page, 'lxml').find(
+    'div', {'id': 'wp-realtime-sitemap-posts'})
+links = articles.findAll('a')
+
+for link in links:
+    article = link.text
+    if collection.find_one({'article': article}) is None:
+        article_url = link.get('href')
+        print('Article "{}", link {}'.format(article, article_url))
+        article_page = urlopen(article_url)
+        print('Loaded article "{}"'.format(article))
+        article_raw = BeautifulSoup(article_page, 'lxml').find(
+            'div', {'class': 'entry'})
+        article_text = article_raw.text.strip()
+        inside_article_urls = article_raw.findAll('a')
+        article_links = []
+        images_links = []
+        for inside_article_url in inside_article_urls:
+            inside_url = inside_article_url.get('href')
+            images = inside_article_url.find('img')
+            if images:
+                image_data = requests.get(inside_url).content
+                image_name = 'img/{}_{}'.format(
+                    str(image_count).zfill(4),
+                    inside_url.split('/')[-1])
+                image_count += 1
+                with open(image_name, 'wb') as handler:
+                    handler.write(image_data)
+                images_links.append({'image_name_local': image_name,
+                                     'image_url': inside_url,
+                                     'image_text': inside_article_url.text
+                                    })
+            else:
+                article_links.append({'inside_url': inside_url,
+                                      'inside_text': inside_article_url.text
+                                     })
+        post = {
+            'article': article,
+            'url': article_url,
+            'text': article_text,
+            'links': article_links,
+            'images': images_links
+        }
+        collection.save(post)
