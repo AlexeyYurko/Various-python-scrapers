afdca7f4328b56f863b9be809007c613ec49c19c
alexey_yurko <a.yurko@gmail.com>
2019-03-07 23:20:23 +0300

    Added: - slightly fast and slightly uncorrect BS4 scraping - argparse

--- whoishiring/whoishiring.py
+++ whoishiring/whoishiring.py
@@ -1,6 +1,11 @@
 """
 get job listing from "Who is hiring" HN thread
+
+TODO allow to modify keywords via command line arguments or config file
+TODO remake block for making html entries
+TODO rewrite API/BS4 block to single function
 """
+import argparse
 import codecs
 import json
 import pickle
@@ -9,14 +14,32 @@ import sqlite3
 import sys
 
 import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 from tqdm import tqdm
+
+
+def create_parser():
+    """create_parser for argparse
+
+    Returns:
+        object: parser
+    """
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        "-t", "--thread", action="store", help="Who is hiring thread number"
+    )
+    argparser.add_argument("-f", "--fast", action="store_true",
+                           help="Fast scraping")
+    return argparser
 
 
 def get_item_url(kid_id):
     """
     make url with kid_id
     """
-    return 'https://hacker-news.firebaseio.com/v0/item/{}.json'.format(str(kid_id))
+    return f'https://hacker-news.firebaseio.com/v0/item/{kid_id}.json'
 
 
 def get_comments(kid_id):
@@ -24,21 +47,21 @@ def get_comments(kid_id):
     get all comments
     """
     result = requests.get(get_item_url(kid_id)).json()
-    return result['text'] if result and 'text' in result else ''
+    return result["text"] if result and "text" in result else ""
 
 
 def get_thread_name(from_thread_id):
     """
     extract name of the thread + month and year
     """
-    story_name = ''
+    story_name = ""
     try:
-        story_name = requests.get(get_item_url(from_thread_id)).json()['title']
+        story_name = requests.get(get_item_url(from_thread_id)).json()["title"]
     except TypeError:
-        print(f'Thread {from_thread_id} non exist.')
+        print(f"Thread {from_thread_id} non exist.")
         exit()
     month_year = re.findall(r'\(([A-Za-z]+ \d+)\)', story_name)[0].lower()
-    short_name = f'whoishiring {month_year}'
+    short_name = f"whoishiring {month_year}"
     return short_name
 
 
@@ -47,7 +70,7 @@ def load_from_json(filename):
     load json file from previous session (if exist)
     """
     try:
-        with open(f'{filename}.json', "r") as file:
+        with open(f"{filename}.json", "r") as file:
             saved_comments = json.load(file)
     except FileNotFoundError:
         saved_comments = []
@@ -58,20 +81,113 @@ def get_kids(thread_id_to_get_kids):
     """
     get kids from story thread
     """
-    story_kids = requests.get(get_item_url(thread_id_to_get_kids)).json()['kids']
+    story_kids = requests.get(get_item_url(
+        thread_id_to_get_kids)).json()["kids"]
     return story_kids
+
+
+def get_all_comments_from_thread(thread):
+    """get_all_comments_from_thread - scrape page by page if link 'more' is
+        available
+
+    Args:
+        thread (int): thread_id
+
+    Returns:
+        [list of soups]: scraped comments
+    """
+
+    url = f"https://news.ycombinator.com/item?id={thread}"
+    not_a_last_page = True
+    total_entries = []
+    while not_a_last_page:
+        print(f"Get page {url}")
+        soup = get_page(url)
+        entries = soup.findAll("div", {"class": "comment"})
+        total_entries += entries
+        have_more_link = soup.findAll("a", {"class": "morelink"})
+        if have_more_link:
+            url_tail = have_more_link[0].get("href")
+            url = f"https://news.ycombinator.com/{url_tail}"
+        else:
+            not_a_last_page = False
+    return total_entries
+
+
+def grab_new_comments_html(comments, entrys):
+    """grab_new_comments_html Get comments from html scrapes via BeautifulSoup.
+                              It's way faster than API, but not very accurate
+
+    Args:
+        comments (list): list of old comments from json file
+        entrys (list): list of comments from thread
+    TODO rewrite db connection
+    """
+    conn = sqlite3.connect("whoishiring.sqlite")
+    cur = conn.cursor()
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS kids(kid INTEGER UNIQE,
+                                                 head BLOB,
+                                                 description BLOB)"""
+    )
+    cur.execute("SELECT kid FROM kids")
+    try:
+        kids_in_base = cur.fetchall()
+    except IndexError:
+        kids_in_base = []
+    stored_kids = list(kid[0] for kid in kids_in_base) if kids_in_base else []
+
+    for entry in entrys:
+        try:
+            text = entry.findAll('span', {'class': 'commtext'})[0]
+        except IndexError:
+            continue
+        try:
+            kid = int(re.findall(r'reply\?id=(\d+)', entry.__str__())[0])
+        except IndexError:
+            kid = 0
+        if kid in stored_kids:
+            continue
+
+        job_head, job_description = '', ''
+        try:
+            job_head = re.findall(
+                r'.+c00">(.+)', text.__str__().split("<p>")[0])[0]
+            job_description = "<p>".join(text.__str__().split(
+                "<p>")[1:]).rstrip("</span>")
+        except IndexError:
+            continue
+        if job_description:
+            if 'We detached' in job_description:
+                continue
+            else:
+                comments.append(
+                    {"head": job_head, "description": job_description})
+                cur.execute(
+                    """INSERT INTO kids
+                        (kid, head, description)
+                        VALUES (?, ?, ?)""",
+                    (kid, job_head, job_description),
+                )
+                conn.commit()
+        else:
+            continue
+    conn.close()
+    return comments
 
 
 def grab_new_comments(comments, all_kids):
     """
     get saved kid_id from base, get only new id
     """
-    conn = sqlite3.connect('whoishiring.sqlite')
+    conn = sqlite3.connect("whoishiring.sqlite")
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS kids(kid INTEGER UNIQE,
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS kids(kid INTEGER UNIQE,
                                                  head BLOB,
-                                                 description BLOB)''')
-    cur.execute('SELECT kid FROM kids')
+                                                 description BLOB)"""
+    )
+    cur.execute("SELECT kid FROM kids")
     try:
         kids_in_base = cur.fetchall()
     except IndexError:
@@ -84,13 +200,15 @@ def grab_new_comments(comments, all_kids):
         job_head = ''
         job_description = ''
         if next_comment:
-            job_head = next_comment.split('<p>')[0]
-            job_description = '<br>'.join(next_comment.split('<p>')[1:])
-            comments.append({'head': job_head, 'description': job_description})
-        cur.execute('''INSERT INTO kids
+            job_head = next_comment.split("<p>")[0]
+            job_description = "<br>".join(next_comment.split("<p>")[1:])
+            comments.append({"head": job_head, "description": job_description})
+        cur.execute(
+            """INSERT INTO kids
                         (kid, head, description)
-                        VALUES (?, ?, ?)''',
-                    (kid, job_head, job_description,))
+                        VALUES (?, ?, ?)""",
+            (kid, job_head, job_description),
+        )
         conn.commit()
     conn.close()
     return comments
@@ -99,61 +217,106 @@ def grab_new_comments(comments, all_kids):
 def make_html(job_listing, filename):
     """
     create simple html from comments with (and without) keyword
-    TODO:
-    - allow to modify keywords via command line arguments or config file
-    - remake block for making html entries
     """
     counter = 0
-    with open('template.html', 'r') as file:
+    with open("template.html", "r") as file:
         template = file.read()
     jobs_block = ''
     for i, entry in enumerate(job_listing, 1):
         entry_text = f"{entry['head']} {entry['description']}"
-        if 'remote' in entry_text or 'REMOTE' in entry_text:
+        if "remote" in entry_text.lower():
             block_start = "<div class='job_entry'>"
-            first_line = f"<div class='job_head'><em>#{i}</em> {entry['head']}</div>"
+            first_line = f"""<div class = 'job_head' > <em >  # {i}</em>
+                                {entry['head']} </div>"""
             jobs_block += f"{block_start}{first_line}{entry['description']}</div>"
             counter += 1
-    with codecs.open(f'{filename}.html', "w", encoding="utf-8") as file:
+    with codecs.open(f"{filename}.html", "w", encoding="utf-8") as file:
         file.write(template.format(filename, jobs_block))
-    print(f'Written to html: {counter} job postings.')
+    print(f"Written to html: {counter} job postings.")
 
 
 def save_to_json(comments_to_save, filename):
     """
     save all comment to json file
     """
-    with open(f'{filename}.json', "w") as file:
+    with open(f"{filename}.json", "w") as file:
         json.dump(comments_to_save, file)
 
 
 def write_thread_id(thread_id_to_save):
-    with open('last_thread.pickle', 'wb') as pickle_out:
+    """write_thread_id Saves last used thread_id in pickle
+
+    Args:
+        thread_id_to_save (int): thread_id to save
+
+    Warning: No check if thread doesn't exist
+    TODO make check for thread
+    """
+
+    with open("last_thread.pickle", "wb") as pickle_out:
         pickle.dump(thread_id_to_save, pickle_out)
+
+
+def get_page(url):
+    """get_page get page via requests and throw it to beautiful soup
+
+    Args:
+        url (string): address
+
+    Returns:
+        [soup]: beautuful soup object
+    """
+
+    response = requests.get(
+        url, headers={"User-Agent": UserAgent().chrome}).content
+    soup = BeautifulSoup(response, "lxml")
+    return soup
+
+
+def scrape(thread):
+    """
+    main block for getting data with BeautifulSoup and a little bit of API ;)
+    """
+    name = get_thread_name(thread)
+    entries = get_all_comments_from_thread(thread)
+    print(f'In thread {thread} with name "{name}" are {len(entries)} records')
+    comments = load_from_json(name)
+    new_comments = grab_new_comments_html(comments, entries)
+    make_html(new_comments, name)
+    save_to_json(new_comments, name)
 
 
 def run(thread):
     """
-    main block
+    main block for getting data with API
     """
-    write_thread_id(thread)
     name = get_thread_name(thread)
     old_comments = load_from_json(name)
     kids = get_kids(thread)
-    print(f'In thread {thread_id} by name "{name}" are {len(kids)} records')
+    print(f'In thread {thread} with name "{name}" are {len(kids)} records')
     new_comments = grab_new_comments(old_comments, kids)
     make_html(new_comments, name)
     save_to_json(new_comments, name)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
+if __name__ == "__main__":
+
+    PARSER = create_parser()
+    ARGS = PARSER.parse_args(sys.argv[1:])
+
+    if not ARGS.thread:
         try:
             # get last used thread_id
-            with open('last_thread.pickle', 'rb') as pickle_in:
-                thread_id = pickle.load(pickle_in)
+            with open("last_thread.pickle", "rb") as pickle_in:
+                THREAD_ID = pickle.load(pickle_in)
         except FileNotFoundError:
-            sys.exit(f'Syntax: {sys.argv[0]} <thread_id>')
+            sys.exit(f"Syntax: {sys.argv[0]} -t <thread_id>")
     else:
-        thread_id = sys.argv[1]
-    run(thread_id)
+        THREAD_ID = ARGS.thread
+
+    write_thread_id(THREAD_ID)
+
+    if ARGS.fast:
+        scrape(THREAD_ID)
+    else:
+        run(THREAD_ID)
