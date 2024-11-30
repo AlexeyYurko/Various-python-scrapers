a011c52ef723e503a380d99f35421d41f657b818
Alexey Yurko <a.yurko@gmail.com>
2024-12-01 01:04:41 +0300

    Update dependencies and enhance Ikea catalog downloader with async functionality
    - refactored Ikea catalog downloader to utilize asynchronous requests with httpx and aiofiles, allowing for concurrent downloads
    - updated the year range in the downloader to include 2022

--- ikea_catalogues/ikea_catalogues.py
+++ ikea_catalogues/ikea_catalogues.py
@@ -3,62 +3,63 @@ Batch download Ikea Catalogues (1950 - 2020 years) from Ikea Museum
 https://ikeamuseum.com/sv/ikea-kataloger/
 """
 
+import asyncio
 import os
 import re
-from concurrent.futures import ThreadPoolExecutor
 
-import requests
+import aiofiles
+import httpx
 from fake_useragent import UserAgent
-from tqdm import tqdm
 
-BASE_URL = 'https://ikeacatalogues.ikea.com/sv-{}/page/1'
-BASE_NAME = 'IKEA {}.pdf'
-OUT_DIR = 'pdfs'
+BASE_URL = "https://ikeacatalogues.ikea.com/sv-{}/page/1"
+BASE_NAME = "IKEA {}.pdf"
+OUT_DIR = "pdfs"
 YEAR_START = 1950
-YEAR_END = 2021
+YEAR_END = 2022
+CHUNK_SIZE = 1024 * 1024
+MAX_CONCURRENT_DOWNLOADS = 20
 
 
-def get_page(url):
-    """get page from url with requests and fake user agent"""
-    response = requests.get(
-        url, headers={'User-Agent': UserAgent().chrome}).text
-    return re.findall(r'"downloadPdfUrl":"(.+.pdf)"', response)[0]
+async def download_file(client, url, filename, semaphore):
+    async with semaphore:
+
+        print(f"Processing {url}")
+        filename_to_save = os.path.join(OUT_DIR, os.path.basename(filename))
+
+        if os.path.exists(filename_to_save):
+            return
+        headers = {"User-Agent": UserAgent().chrome}
+        try:
+            response = (await client.get(url, headers=headers)).text
+            pdf_url = re.findall(r'"downloadPdfUrl":"(.+.pdf)"', response)[0]
+
+            async with client.stream("GET", pdf_url) as response:
+                async with aiofiles.open(filename_to_save, "wb") as f:
+                    async for chunk in response.aiter_bytes(chunk_size=CHUNK_SIZE):
+                        await f.write(chunk)
+        except Exception as e:
+            print(f"Error while processing {url} {e}")
+            return
 
 
-def download_file(urls_filename):
-    """download file with check for existing file"""
-    url, filename = urls_filename
-    filename_to_save = os.path.join(
-        OUT_DIR, os.path.basename(filename))
-
-    if os.path.exists(filename_to_save):
-        return
-
-    try:
-        pdf_url = get_page(url)
-        file_data = requests.get(
-            pdf_url, headers={'User-Agent': UserAgent().chrome}).content
-    except:
-        return
-
-    try:
-        with open(filename_to_save, 'wb') as handler:
-            handler.write(file_data)
-    except FileExistsError:
-        return
-
-
-def run():
-    """main function"""
+async def flow():
     if not os.path.exists(OUT_DIR):
         os.mkdir(OUT_DIR)
 
-    pages = [(BASE_URL.format(year), BASE_NAME.format(year))
-             for year in range(YEAR_START, YEAR_END + 1)]
+    url_filenames = [
+        (BASE_URL.format(year), BASE_NAME.format(year))
+        for year in range(YEAR_START, YEAR_END + 1)
+    ]
 
-    with ThreadPoolExecutor() as executor:
-        _ = list(tqdm(executor.map(download_file, pages), total=len(pages)))
+    semaphore = asyncio.Semaphore(MAX_CONCURRENT_DOWNLOADS)
+
+    async with httpx.AsyncClient() as client:
+        tasks = [
+            download_file(client, url, filename, semaphore)
+            for url, filename in url_filenames
+        ]
+        await asyncio.gather(*tasks)
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    asyncio.run(flow())
